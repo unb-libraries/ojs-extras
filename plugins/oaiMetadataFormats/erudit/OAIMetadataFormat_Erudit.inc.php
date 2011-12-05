83ee6dbad3a4bcb1ff8fd4b2e6281ca295b65749
J Whitney <jen.j.whitney@gmail.com>
2011-12-05 11:06:08 -0400

    Refactor code for better maintainability. Add comments to describe how Erudit XML files are located.  Replace hard-coded 'find' command with a reasonably efficient recursive glob.
    Fixes #10.

--- plugins/oaiMetadataFormats/erudit/OAIMetadataFormat_Erudit.inc.php
+++ plugins/oaiMetadataFormats/erudit/OAIMetadataFormat_Erudit.inc.php
@@ -18,6 +18,10 @@
  */
 
 
+// Location of Erudit source XML
+// @todo plugin configuration setting?
+define ('ERUDIT_SOURCE_DIR', '/etcdata/journals/etc_journals');
+
 class OAIMetadataFormat_Erudit extends OAIMetadataFormat {
 
   /**
@@ -27,27 +31,111 @@ class OAIMetadataFormat_Erudit extends OAIMetadataFormat {
 
     $journal =& $record->getData('journal');
     $article =& $record->getData('article');
+    
+    // Begin OAI response
+    $response = '';
 
-    // Get a list of files associated with this article
-    $articleId = $article->getId();
+    // Add URIs for each of the article's galleys
+    foreach ($article->getGalleys() as $galley) {
+      $response .= 
+        $this->formatSelfUri(
+          Request::url($journal->getPath(), 'article', 'view',  array($article->getBestArticleId(), $galley->getId()) ),
+          $galley->getFileType()
+        );
+    }
+
+    // Next, determine the Erudit XML filename we expect for this article
+    $eruditXmlFileName = $this->getEruditXmlFileName($article->getId());
+    
+    if (! $eruditXmlFileName) {
+      trigger_error("Can't identify Érudit XML file for article ID $articleId");
+      $response .= $this->formatError("No metadata available.");
+      return $response;
+    }
+
+    // Locate the Erudit XML file 
+    $eruditXmlFiles = $this->findFiles(ERUDIT_SOURCE_DIR, $eruditXmlFileName);
+
+    if (! sizeof($eruditXmlFiles)) {
+      trigger_error("Érudit XML file '$eruditXmlFileName' not found for article ID $articleId");
+      $response .= $this->formatError("No metadata available.");
+      return $response;
+    }
+    
+    // @todo what if there are multiple files?
+    // For now, grab the first file in list
+    $eruditXml = file_get_contents($eruditXmlFiles[0]);
+
+    if (! $eruditXml) {
+      trigger_error("Can't read Érudit XML file '$eruditXmlFileName' for article ID $articleId");
+      $response .= $this->formatError("No metadata available.");
+      return $response;
+    }
+    
+    // Add Erudit XML to response
+    $response .= $this->formatEruditArticle($eruditXml);
+    
+    // That's it.
+    return $response;  
+	}
+	
+	/**
+	* Element <self-uri> identifies alternate versions of this document: in this
+	* case, HTML and/or PDF galleys.
+	*/
+	private function formatSelfUri($href, $contentType) {
+    $selfUri = '<self-uri ' . 
+                  'xmlns:xlink="http://www.w3.org/1999/xlink" ' . 
+                  'content-type="' . htmlspecialchars(Core::cleanVar($contentType)) . '" ' .
+                  'xlink:href="' . htmlspecialchars(Core::cleanVar($href)) . '" ' .
+                '/>';
+                
+    return $selfUri;
+  }
+  
+  /** 
+   * Element <article> is Erudit Article 3.0 DTD -compliant XML
+   */
+ private function formatEruditArticle($eruditXml) {
+   // Strip XML, doc type declarations, etc. from XML source, and 
+   // return <article> element
+   return preg_replace("{^.*?<article}is", "<article", $eruditXml);
+ }
+ 
+  /**
+   * Element <error> adds an error message to the response
+   */
+  private function formatError($errorMessage) {
+    return "<error>$errorMessage</error>";
+  }
+  
+  // Erudit XML files *should* follow the naming convention used for PDF and
+  // HTML galley filenames, i.e.,
+  // [journal_id][vol](_[issue](_[part])?)?[article_type_id][article_no].[pdf|html]
+  // 
+  // E.g., 
+  //  ageo47art07.html     => AGEO Vol. 47, article 07, HTML galley
+  //  scl36_1art05.pdf     => SCL Vol. 36, Issue 1, article 05, PDF galley
+  //  tric30_1_2art03.html => TRIC Vol. 30, Issue 1, part 2, article 03, HTML galley
+  //
+  // For the galleys above, we should be able to locate these XML files:
+  //  ageo47art07.xml
+  //  scl36_1art05.xml
+  //  tric30_1_2art03.xml
+  //
+  private function getEruditXmlFileName($articleId) {
+
     $articleFileDao =& DAORegistry::getDAO('ArticleFileDAO');
     $articleFiles = $articleFileDao->getArticleFilesByArticle($articleId);
-    
-    // Public PDF and HTML galleys of published articles *should* follow the
-    // same naming convention used to name Erudit XML files, i.e.,
-    // [journal_id][vol](_[issue](_[part])?)?[article_type_id][article_no].[pdf|html]
-    // 
-    // E.g., 
-    // ageo47art07.html     => AGEO Vol. 47, article 07, HTML galley
-    // scl36_1art05.pdf     => SCL Vol. 36, Issue 1, article 05, PDF galley
-    // tric30_1_2art03.html => TRIC Vol. 30, Issue 1, part 2, article 03, HTML galley
-    
-    // Examine files with type = 'public' to find a candidate galley upon
-    // which to base the search for the corresponding Erudit XML file. First
-    // parenthesized subpattern captures filename excluding extension 
+
+    // First parenthesized subpattern captures filename excluding extension.    
     $fileNameRegExp = "/^([A-Za-z]+\d+_?(\d+)?(_\d+)*[a-z]+\d+)\.(pdf|html)$/";
 
+    // Examine files with type = 'public' to find a candidate galley upon
+    // which to base the search for the corresponding Erudit XML file. 
+    
     $eruditXmlFileName = '';
+    
     foreach ($articleFiles as $articleFile) {
       // We're only interested in public galleys
       if ($articleFile->getType() == 'public') {
@@ -61,59 +149,24 @@ class OAIMetadataFormat_Erudit extends OAIMetadataFormat {
           break;
         }
       }
-      
+
     } // end foreach ($articleFiles as $articleFile)
     
-    if (! $eruditXmlFileName) {
-      // Ugly but temporarily useful;
-      $error = "Can't identify Érudit XML file for article ID $articleId";
-      error_log($error);
-      return "<error>$error</error>";
-    }
-
-    // Find the XML file 
-    $eruditXmlFiles = 
-      preg_split("{\n}", 
-        trim(
-          shell_exec('/usr/bin/find /home/journals/etc_journals -wholename ' . escapeshellarg("*/XML/$eruditXmlFileName") . ' -print')
-        )
-      );
+    return $eruditXmlFileName;
+  }
+  
+  // Return a list of file names matching $pattern in $path or its subdirectories
+  private function findFiles($dir, $pattern) {
+    $dir = escapeshellcmd($dir);
+    $files = glob("$dir/$pattern");
     
-    if (! sizeof($eruditXmlFiles)) {
-      // Still ugly, still useful
-      $error = "Érudit XML file '$eruditXmlFileName' not found for article ID $articleId";
-      error_log($error);
-      return "<error>$error</error>";
+    // Get sub-directories in current directory. Ignore hidden directories.
+    foreach (glob("$dir/*", GLOB_ONLYDIR) as $subdir) {
+      $subdirFiles = $this->findFiles($subdir, $pattern);  // resursive call
+      $files = array_merge($files, $subdirFiles); // merge array with files from subdirectory
     }
     
-    // Read the XML file
-    $eruditXml = file_get_contents($eruditXmlFiles[0]);
-    
-    if (! $eruditXml) {
-      // See above.
-      $error = "Can't read Érudit XML file '$eruditXmlFileName' for article ID $articleId";
-      error_log($error);
-      return "<error>$error</error>";
-    }
-    
-    // Extract the <article> element from the Erudit XML
-    $eruditArticleXml = preg_replace("{^.*?<article}is", "<article", $eruditXml);
-
-    // Begin the response with URIs for each of the article's galleys
-    $response = '';
-    foreach ($article->getGalleys() as $galley) {
-      $response .= '<self-uri ' . 
-                    'xmlns:xlink="http://www.w3.org/1999/xlink" ' . 
-                    'content-type="' . htmlspecialchars(Core::cleanVar($galley->getFileType())) . '" ' .
-                    'xlink:href="' . htmlspecialchars(Core::cleanVar(Request::url($journal->getPath(), 'article', 'view', array($article->getBestArticleId(), $galley->getId())))) . '" ' .
-                    '/>' . "\n";
-    }
-    
-    // Append the article XML
-    $response .= $eruditArticleXml;
-    
-    // That's it.
-    return $response;  
-	}
+    return $files;
+  }
 }
 ?>
